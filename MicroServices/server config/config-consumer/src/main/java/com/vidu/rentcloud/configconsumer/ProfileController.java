bf06edefdb564c830adf6c3c04b72e59b1cd5438
vidurshan <vidurshan1996>
2022-04-10 20:54:19 +0530

    local and git config

---
+++ MicroServices/server config/config-consumer/src/main/java/com/vidu/rentcloud/configconsumer/ProfileController.java
@@ -0,0 +1,28 @@
+package com.vidu.rentcloud.configconsumer;
+
+
+import com.vidu.rentcloud.configconsumer.model.MemberProfileConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ProfileController {
+
+
+    @Autowired
+    MemberProfileConfiguration memberProfileConfiguration;
+
+
+    @RequestMapping("/profile")
+    public String getConfig(Model model){
+
+        model.addAttribute("model",memberProfileConfiguration.getDefaultModel());
+        model.addAttribute("min",memberProfileConfiguration.getMinRentPeriod());
+        return "mprofile";
+
+    }
+
+}
