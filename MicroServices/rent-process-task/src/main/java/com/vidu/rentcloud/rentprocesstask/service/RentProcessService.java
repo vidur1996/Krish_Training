631352db9056e758866c1b64d059b2de61a49671
vidurshan <vidurshan1996>
2022-04-18 22:28:46 +0530

    async service

---
+++ MicroServices/rent-process-task/src/main/java/com/vidu/rentcloud/rentprocesstask/service/RentProcessService.java
@@ -0,0 +1,5 @@
+package com.vidu.rentcloud.rentprocesstask.service;
+
+public interface RentProcessService {
+    boolean validateDL(String dlNumber);
+}
---
+++ MicroServices/rent-process-task/src/main/java/com/vidu/rentcloud/rentprocesstask/service/RentProcessServiceImpl.java
@@ -0,0 +1,15 @@
+package com.vidu.rentcloud.rentprocesstask.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RentProcessServiceImpl implements RentProcessService {
+
+
+
+    @Override
+    public boolean validateDL(String dlNumber) {
+        return dlNumber.length() > 5;
+    }
+
+}
